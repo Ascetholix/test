71e6e6ec3aa37052ce1e8522b46804a688d4e56a
Sangak <96950842+Ascetholix@users.noreply.github.com>
2022-09-07 18:22:40 +0300

    Добавление в int

---
+++ test4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Введите 5ти значное число :");
+int num = Convert.ToInt32(Console.ReadLine());
+int t = num;
+int sum = 0;
+int r = 0;
+
+while (num != 0)
+{
+    r = num % 10;
+    num = num / 10;
+    sum = sum * 10 + r;
+}
+if (t == sum)
+{
+    Console.WriteLine($"{t}--->Да");
+}
+else
+{
+    Console.WriteLine($"{t}--->Нет");
+}
